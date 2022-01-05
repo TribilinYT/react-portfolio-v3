a889bb48281057d20e1abb367ce2920272b4512a
Cristhian A Luna <jobs.cluna@gmail.com>
2022-01-05 13:44:23 -0500

    fix: change experience <@devcluna>

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,29 +1,29 @@
 export const experience = [
     {
         id: 0,
-        title: "Frontend Developer",
-        time: "July - December 2019",
+        title: " Fullstack Developer",
+        time: "March 2021 - until Now",
         status: true,
         business: "Freelance",
         description: [
             {
                 idDescription: 0,
-                text:"Contributed with vanilla wireframes that allow UX team to get store ideas that finally implemented to the production version."
+                text:"UI development with support for all browsers, and responsiveness for any device"
             },
             {
                 idDescription: 1,
-                text:"Participated in software requirements specification; some of them were used to the final version."
+                text:"Design, development and management of SQL and noSQL databases (PostgreSQL, MySQL and Node.js)"
             },
             {
                 idDescription: 2,
-                text:"Developed some modules like user Login, Register, and Rest API for user authentication"    
+                text:"Development, encryption of Restful API routes (endpoints) for the requests of client apps. "    
             },
         ]                            
     },
     {
         id: 1,
-        title: "Frontend Developer Coop",
-        time: "July - December 2015",
+        title: "Fullstack Developer (MERN) Coop",
+        time: "May 2020 - Sept 2020",
         status: false,
         business: "BESE",
         description: [
@@ -39,26 +39,34 @@ export const experience = [
                 idDescription: 2,
                 text:"Developed some modules like user Login, Register, and Rest API for user authentication"    
             },
+            {
+                idDescription: 3,
+                text:"Participated in the development of the Restful API, and set the endpoints & server requests. "    
+            },
+            {
+                idDescription: 4,
+                text:"Created the user journey for the subsequent management of navigation and contente"    
+            },
         ]                         
     },
     {
         id: 2,
         title: "Frontend Developer",
-        time: "July - December 2015",
+        time: "March 2019 - Feb  2020",
         status: false,
         business: "Kaninos",
         description: [
             {
                 idDescription: 0,
-                text:"Worked with a team of 5 developers to build a POS software for a local veterinary health."
+                text:"Created a desktop application that syncs data from the server, and works at the same time as the browser version. "
             },
             {
                 idDescription: 1,
-                text:"Helped to create the user journey that allow to the designers create the UI."
+                text:"Developed POS service , which included billing, inventory management, and an overview of income and expensese"
             },
             {
                 idDescription: 2,
-                text:"Participated in the development of desktop version that finally was used in production version."    
+                text:"Built a registry containing pet information to make it easier to track their vaccines, medications, and/or veterinary treatments. "    
             },
         ]                      
     }
